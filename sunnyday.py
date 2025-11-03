906f146baf44a858945b08fb8a969edd525bb880
dineshmurani <51493204+dineshmurani@users.noreply.github.com>
2025-11-03 13:57:46 -0800

    S59 App11 Weather Data 236 Adding Docs to your Library

--- sunnyday.py
+++ sunnyday.py
@@ -8,7 +8,29 @@ url = "http://api.openweathermap.org/data/2.5/forecast?lat=40.1&lon=3.48&appid=c
 # print(r.json())
 
 class Weather:
+    """Creates a Weather object getting an apikey as input and either a city name or lat and lon coordinates.
 
+    Package use example:
+    # Create a weather object using a City name:
+    # The api key below is not guranateed to work.
+    # Get your own apikey from https://openweathermap.org
+    # And wait a couple of hours for the apikey to be activated.
+
+    >>>> weather1 = Weather(apikey="apikey", city="madrid")
+
+    # Using latitude and longitude coordinates
+
+    >>>> weather1 = Weather(apikey="apikey", lat=41.1, lon= -4.1)
+
+    # Get complete weather data for the next 12 hours:
+
+    >>>> weather1.next_12()
+
+    # Simplified data for the next 12 hours:
+
+    >>>> weather1.next_12_simplified()
+
+    """
     def __init__(self, apikey, city=None, lat=None, lon=None):
         if city:
             url = f"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={apikey}&units=imperial"
