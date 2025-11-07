b90ffc0c920597addbb5c83f4b42b3fdb1061cb2
dineshmurani <51493204+dineshmurani@users.noreply.github.com>
2025-11-07 10:48:03 -0800

    S59 App11 Weather Data 237 Preparing and Testing the Library

--- sunnyday.py
+++ sunnyday.py
@@ -1,4 +1,4 @@
-import requests, pprint
+import requests
 
 
 # apikey ="c0c7cb62c5e8740501f9c83e45bd7dd7"
@@ -36,10 +36,10 @@ class Weather:
             url = f"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={apikey}&units=imperial"
             r = requests.get(url)
             self.data = r.json()
-            # print(self.data)
+            
         elif lat and lon:
             url = f"http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}8&appid={apikey}&units=imperial"
-            # print(url)
+
             r = requests.get(url)
             self.data = r.json()
         else:
@@ -54,7 +54,6 @@ class Weather:
         Returns 3-hour data for the next 12 hours as dict.
         :return:
         """
-        # print(self.data)
         return self.data['list'][:4]
 
     def next_12h_simplified(self):
@@ -68,9 +67,9 @@ class Weather:
             simple_data.append((dicty['dt_txt'], dicty['main']['temp'], dicty['weather'][0]['description']))
         return simple_data
 
-# weather = Weather(apikey="c0c7cb62c5e8740501f9c83e45bd7dd7", city="San Jose")
-weather = Weather(apikey="c0c7cb62c5e8740501f9c83e45bd7dd7", city="Madrid", lat = 4.1, lon = 4.5)
-# print(weather.data)
-pprint.pprint(weather.next_12h_simplified())
+# # weather = Weather(apikey="c0c7cb62c5e8740501f9c83e45bd7dd7", city="San Jose")
+# weather = Weather(apikey="c0c7cb62c5e8740501f9c83e45bd7dd7", city="Madrid", lat = 4.1, lon = 4.5)
+# # print(weather.data)
+# pprint.pprint(weather.next_12h_simplified())
 
 
